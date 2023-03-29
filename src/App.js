624616bf9f2dc01b31839357bf1e5e4cfa270f17
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-03-29 15:15:16 +0530

    modified app.js with code spliting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { Suspense } from 'react';
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import './App.css'
-import UsersList from './components/UserList';
+import LoadingSpinner from './components/LoadingSpinner';
+
+const UsersList = React.lazy(() => import('./components/UserList.jsx'));
+
 export default function App() {
 
   return (
     <div className="container">
+      <Suspense fallback={<LoadingSpinner/>}>
       <Routes>
         <Route path='/' element={<UsersList />} />
       </Routes>
+      </Suspense>
     </div>
   );
 }
